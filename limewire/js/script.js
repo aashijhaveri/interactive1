b8a0cd717207f728a3c083421635435cb014e1bb
Aashi Jhaveri <aashijhaveri@Aashis-MBP.lan>
2019-04-11 08:51:56 +0200

    limewire

--- limewire/js/script.js
+++ limewire/js/script.js
@@ -1,10 +1,10 @@
- $(function(){
-   $(".img-swap").live('click', function() {
-     if ($(this).attr("class") == "img-swap") {
-       this.src = this.src.replace("_off","_on");
-     } else {
-       this.src = this.src.replace("_on","_off");
-     }
-     $(this).toggleClass("on");
-   });
- });
+ 
+
+$(document).ready(function(){
+	$(".limewire").click(function() {
+		$("div").show();
+		$(".limewire").hide();
+
+	});
+});
+
